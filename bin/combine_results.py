5b1e9cedff840d0e804752ebaa2d03a425dae4fa
Marlon James <marlon.james@garmin.com>
2020-11-24 15:10:46 -0800

    Use testcase name not testsuite name

--- bin/combine_results.py
+++ bin/combine_results.py
@@ -82,7 +82,7 @@ def main():
                     rc = 1
                 print("Failure in testsuite: '%s' classname: '%s' testcase: '%s' with parameters '%s'" % (testsuite.get('name'), testcase.get('classname'), testcase.get('name'), testsuite.get('package')))
                 if os.getenv('GITHUB_ACTIONS') is not None:
-                    print("::error file={}.py::Test {} failed".format(testcase.get('classname'), testsuite.get('name')))
+                    print("::error file={}.py::Test {} failed".format(testcase.get('classname'), testcase.get('name')))
 
     print("Ran a total of %d TestSuites and %d TestCases" % (testsuite_count, testcase_count))
 
